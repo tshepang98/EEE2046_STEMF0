82eae1e68ed577cb96f5d97d4bed3854d9d97db7
tshepang98 <60628006+tshepang98@users.noreply.github.com>
2023-05-10 12:49:30 +0200

    Add files via upload
    complete working program

--- main.c
+++ main.c
@@ -1,154 +1,222 @@
+//********************************************************************
+//*                    EEE2046F C template                           *
+//*==================================================================*
+//* WRITTEN BY: Tshepang Mathlore 	                 		                         *
+//* DATE CREATED: 10/05/2023                                                   *
+//* MODIFIED:                                                        *
+//*==================================================================*
+//* PROGRAMMED IN: Atollic True Studio                               *
+//* TARGET:        STM32F0                                           *
+//*==================================================================*
+//* DESCRIPTION:                                                     *
+//*                                                                  *
+//********************************************************************
+// INCLUDE FILES
+//====================================================================
 #include "stm32f0xx.h"
-
-#define DELAY1 1000000
-#define DELAY2 1000000
-
-typedef uint8_t flag_t;
-
+#include <stdint.h>
+//====================================================================
+// GLOBAL CONSTANTS
+//====================================================================
+#define DELAY1 0
+#define DELAY2 500000
 #define TRUE 1
 #define FALSE 0
-
-volatile uint32_t delayCount;
-
+ 
+ 
+//====================================================================
+// GLOBAL VARIABLES
+//====================================================================
+typedef uint8_t flag_t;
+uint16_t bitpattern1 = 0;		// Sets the ODR to turn of LEDs
+uint16_t bitpattern2 = 65535;	// set the first 8 bits of ODR to 11111111 which will display this pattern when called
+uint16_t bitpattern3 = 170;     // set the first 8 bits of ODR to 10101010 which will display this pattern when called
 flag_t startFlag = FALSE;
 flag_t firstFlag = FALSE;
 flag_t secondFlag = FALSE;
 flag_t thirdFlag = FALSE;
-
-void initGPIO(void) {
-    // Enable GPIOA and GPIOB peripheral clocks
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
-
-    // Configure LED pins (B0-B7) as outputs
-    GPIOB->MODER |= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 |
-                    GPIO_MODER_MODER2_0 | GPIO_MODER_MODER3_0 |
-                    GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0 |
-                    GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0;
-
-    // Configure push button pins (A0-A3) as inputs
-    GPIOA->MODER &= ~(GPIO_MODER_MODER0 | GPIO_MODER_MODER1 |
-                      GPIO_MODER_MODER2 | GPIO_MODER_MODER3);
-
-    // Enable pull-up resistors for push buttons
-    GPIOA->PUPDR |= GPIO_PUPDR_PUPDR0_0 | GPIO_PUPDR_PUPDR1_0 |
-                    GPIO_PUPDR_PUPDR2_0 | GPIO_PUPDR_PUPDR3_0;
+ 
+uint16_t readInput;
+//====================================================================
+// FUNCTION DECLARATIONS
+//====================================================================
+void initGPIO(void);
+void delay(void);
+void firstDisplay(void);
+void secondDisplay(void);
+void thirdDisplay(void);
+void checkPB(void);
+void display(void);
+//====================================================================
+// MAIN FUNCTION
+//====================================================================
+int main (void)
+{
+	for(;;){
+ 
+		initGPIO(); //enable registers
+		display();
+	}
+}							// End of main
+ 
+//====================================================================
+// FUNCTION DEFINITIONS
+//====================================================================
+ 
+void initGPIO(void){
+	RCC->AHBENR = 0x00000014;		// Reset the AHBENR before updating it
+	//GPIOA->MODER = 0;
+	RCC->AHBENR |= ((uint32_t)(1<<17) | (uint32_t)(1<<18));		// Connects the clock to PORT A and B
+	GPIOA->MODER &=~ (GPIO_MODER_MODER0 | GPIO_MODER_MODER1 | GPIO_MODER_MODER2 | GPIO_MODER_MODER3); // Set PORT(0-4) to Output Mode
+	GPIOA->PUPDR |= 0x00000055;		// Enable Pull Up registers for PORT A
+	GPIOB->MODER |= 0x00005555;		// set Push Buttons to digital output PORTB
+	GPIOB->ODR = 0;                 // Disable all LEDs
+	readInput = GPIOA->IDR;
+	GPIOA->IDR = 0;
 }
-
-void delay(void) {
-    for (delayCount = 0; delayCount < DELAY1; delayCount++) {
-        for (uint32_t i = 0; i < DELAY2; i++) {
-            // Delay loop
-        }
+ 
+void delay(void){
+	for(int i = 0;i < DELAY1;i++);
+	for(int j = 0;j < DELAY2;j++);
+}
+ 
+void firstDisplay(void){
+ 
+		GPIOB->ODR = bitpattern1;
+		delay();
+		GPIOB->ODR = bitpattern2;
+		delay();
+		GPIOB->ODR = bitpattern3;
+		delay();
+}
+ 
+void secondDisplay(void){
+		for(int i = 0; i < 8; ++i){
+			GPIOB->ODR = 0; 	// Reset the ODR register
+			//delay();
+			switch(i){
+			case 0:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_0;
+				delay();
+				break;
+			case 1:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_1;
+				delay();
+				break;
+			case 2:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_2;
+				delay();
+				break;
+			case 3:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_3;
+				delay();
+				break;
+			case 4:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_4;
+				delay();
+				break;
+			case 5:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_5;
+				delay();
+				break;
+			case 6:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_6;
+				delay();
+				break;
+			case 7:
+				checkPB();
+				if (!secondFlag) break;
+				GPIOB->ODR |= GPIO_ODR_7;
+				delay();
+				break;
+			}
+		}
+}
+ 
+void thirdDisplay(void){
+	int bits = 0;        // Hold the current bit-counter
+	GPIOB->ODR = 0;		// initially all the LED are off
+	while(thirdFlag){
+		delay();
+		GPIOB->ODR += 1;		// The counter must start counting up from 0 and should keep counting up in increments of 1 until the maximum value is displayed on the LEDS
+		if (bits == 255){		// the maximum value for 8 LEDs
+			bits = 0;			// reset the counter
+			continue;
+		}
+		bits++;
+		checkPB();
+	}
+}
+ 
+void checkPB(void)
+{
+ 
+    if ((GPIOA -> IDR & GPIO_IDR_0) == 0)     // is SW0 pressed?
+    {
+        startFlag = TRUE;
+        firstFlag = FALSE;
+        secondFlag = FALSE;
+        thirdFlag = FALSE;
     }
-}
-
-void firstDisplay(void) {
-    while (startFlag == TRUE) {
-        GPIOB->ODR = 0xAA;  // Display bitpattern1
-        delay();
-
-        GPIOB->ODR = 0x55;  // Display bitpattern2
-        delay();
-
-        GPIOB->ODR = 0xFF;  // Display bitpattern3
-        delay();
+ 
+    else if (( GPIOA -> IDR & GPIO_IDR_1) == 0) // is SW1 pressed?
+    {
+        startFlag = TRUE;
+        firstFlag = TRUE;
+        secondFlag = FALSE;
+        thirdFlag = FALSE;
     }
-}
-
-void secondDisplay(void) {
-    while (startFlag == TRUE) {
-        GPIOB->ODR = 0x01;  // Display Step 1
-        delay();
-
-        GPIOB->ODR = 0x02;  // Display Step 2
-        delay();
-
-        GPIOB->ODR = 0x04;  // Display Step 3
-        delay();
-
-        GPIOB->ODR = 0x08;  // Display Step 4
-        delay();
-
-        GPIOB->ODR = 0x10;  // Display Step 5
-        delay();
-
-        GPIOB->ODR = 0x20;  // Display Step 6
-        delay();
-
-        GPIOB->ODR = 0x40;  // Display Step 7
-        delay();
-
-        GPIOB->ODR = 0x80;  // Display Step 8
-        delay();
+ 
+    else if (( GPIOA -> IDR & GPIO_IDR_2) == 0) // is SW2 pressed?
+    {
+        startFlag = TRUE;
+        firstFlag = FALSE;
+        secondFlag = TRUE;
+        thirdFlag = FALSE;
     }
-}
-
-void thirdDisplay(void) {
-    uint8_t counter = 0;
-
-    while (startFlag == TRUE) {
-        GPIOB->ODR = counter;  // Display counter value
-        delay();
-
-        counter++;
-
-        if (counter == 0) {
-            // Reset the counter if it reaches its maximum value
-            counter = 0;
-        }
+ 
+    else if (( GPIOA -> IDR & GPIO_IDR_3) == 0) // is SW3 pressed?
+    {
+        startFlag = TRUE;
+        firstFlag = FALSE;
+        secondFlag = FALSE;
+        thirdFlag = TRUE;
     }
+ 
 }
-void checkPB(void) {
-    // Check the status of SW0, SW1, SW2, and SW3 buttons
-    // Update the corresponding flags (startFlag, firstFlag, secondFlag, thirdFlag) accordingly
-
-    // Assuming GPIOA pins are used for the push buttons
-    uint8_t sw0 = GPIOA->IDR & GPIO_IDR_IDR_0_Msk; // Read SW0 status
-    uint8_t sw1 = GPIOA->IDR & GPIO_IDR_IDR_1_Msk; // Read SW1 status
-    uint8_t sw2 = GPIOA->IDR & GPIO_IDR_IDR_2_Msk; // Read SW2 status
-    uint8_t sw3 = GPIOA->IDR & GPIO_IDR_IDR_3_Msk; // Read SW3 status
-
-    // Update the flags based on the button status
-    startFlag = (sw0 == 0) ? TRUE : FALSE;
-    firstFlag = (sw1 == 0) ? TRUE : FALSE;
-    secondFlag = (sw2 == 0) ? TRUE : FALSE;
-    thirdFlag = (sw3 == 0) ? TRUE : FALSE;
+ 
+void display(void){
+	checkPB();
+	if (startFlag && !firstFlag && !secondFlag && !thirdFlag){
+		GPIOB->ODR |= GPIO_ODR_0;
+		delay();
+	}
+	if(firstFlag){
+		firstDisplay();
+	}if(secondFlag){
+		secondDisplay();
+	}if(thirdFlag){
+		thirdDisplay();
+	}
+ 
 }
-
-
-void display(void) {
-    if (startFlag == TRUE) {
-        // Call the appropriate display function based on the active flag
-        if (firstFlag == TRUE) {
-            firstDisplay();
-        }
-        else if (secondFlag == TRUE) {
-            secondDisplay();
-        }
-        else if (thirdFlag == TRUE) {
-            thirdDisplay();
-        }
-        else {
-            // No active flag, do nothing or handle the default case
-        }
-    }
-    else {
-        // startFlag is FALSE, do nothing or handle the case when the display should be turned off
-    }
-}
-
-
-int main(void) {
-    initGPIO();  // Initialize GPIO
-
-    // Main loop
-    while (1) {
-        // Check if SW0, SW1, SW2, or SW3 is pressed
-        checkPB();
-
-        // Display the appropriate LED pattern based on the flags
-        display();
-    }
-
-    return 0;
-}
+ 
+ 
+ 
+//********************************************************************
+// END OF PROGRAM
+//********************************************************************
